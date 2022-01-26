5570b886af0360458a27644731544bb2920dfe68
GreenArt <GreenArt@DESKTOP-758VK1T>
2022-01-26 16:59:56 +0900

    황준호 - 이름 getset추가

--- SpringJun/src/main/java/org/jun/domain/MemberDTO.java
+++ SpringJun/src/main/java/org/jun/domain/MemberDTO.java
@@ -21,74 +21,102 @@ public class MemberDTO {
 	public String getbirthyymmdd() {
 		return birth;
 	}
-	
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getBirth() {
 		return birth;
 	}
+
 	public void setBirth(String birth) {
 		this.birth = birth;
 	}
+
 	public String getYy() {
 		return yy;
 	}
+
 	public void setYy(String yy) {
 		this.yy = yy;
 	}
+
 	public String getMm() {
 		return mm;
 	}
+
 	public void setMm(String mm) {
 		this.mm = mm;
 	}
+
 	public String getDd() {
 		return dd;
 	}
+
 	public void setDd(String dd) {
 		this.dd = dd;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getJobrank() {
 		return jobrank;
 	}
+
 	public void setJobrank(String jobrank) {
 		this.jobrank = jobrank;
 	}
+
 	@Override
 	public String toString() {
-		return "MemberDTO [id=" + id + ", password=" + password + ", birth=" + birth + ", yy=" + yy + ", mm=" + mm
-				+ ", dd=" + dd + ", gender=" + gender + ", email=" + email + ", phone=" + phone + ", jobrank=" + jobrank
-				+ "]";
+		return "MemberDTO [id=" + id + ", password=" + password + ", name=" + name + ", birth=" + birth + ", yy=" + yy
+				+ ", mm=" + mm + ", dd=" + dd + ", gender=" + gender + ", email=" + email + ", phone=" + phone
+				+ ", jobrank=" + jobrank + "]";
 	}
+	
 	
 	
 }
