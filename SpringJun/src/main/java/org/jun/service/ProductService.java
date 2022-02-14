63948b76158437d3081552c8f4e245890ea0185a
GreenArt <GreenArt@DESKTOP-758VK1T>
2022-02-14 14:48:09 +0900

    황준호 - 컨트롤러 수정

---
+++ SpringJun/src/main/java/org/jun/service/ProductService.java
@@ -0,0 +1,14 @@
+package org.jun.service;
+
+import java.util.ArrayList;
+
+
+import org.jun.domain.Criteria;
+import org.jun.domain.ProductDTO;
+
+public interface ProductService {
+	
+	public void productWrite(ProductDTO Cdto);
+	
+
+}
